8a452adb349f5f2681baa21e63953873dafc1d1f
Robert Aradei <aradeirobert@gmail.com>
2024-03-06 20:04:30 +0200

    Added rating validation

--- CustomerFeedbackAPI/Models/FeedbackCreate.cs
+++ CustomerFeedbackAPI/Models/FeedbackCreate.cs
@@ -1,10 +1,13 @@
 ﻿using RestaurantLibrary.DTOs;
+using System.ComponentModel.DataAnnotations;
 
 namespace CustomerFeedbackAPI.Models
 {
     public class FeedbackCreate
     {
         public string Comments { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
         public byte Rating { get; set; }
 
         public CustomerFeedback ToDTO(int restaurantId)
